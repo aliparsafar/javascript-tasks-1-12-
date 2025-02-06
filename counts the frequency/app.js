76b8661be1ee634f4521960a0f75bdaf27d65d3f
aliparsafar <aliparsafar.code@gmail.com>
2025-02-06 18:09:01 +0330

    add js file (completed)

--- counts the frequency/app.js
+++ counts the frequency/app.js
@@ -1,1 +1,14 @@
 
+function countFrequency(arr) {
+    const frequency = {};
+    
+    arr.forEach(item => {
+        frequency[item] = (frequency[item] || 0) + 1;
+    });
+    
+    return frequency;
+}
+const inputArray = ['a', 'b', 'a', 'c', 'b', 'a'];
+const result = countFrequency(inputArray);
+
+console.log(result);
